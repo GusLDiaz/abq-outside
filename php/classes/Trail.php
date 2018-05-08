7854eb7f9c4b8e699d0cedf1a1bbef9c99ef3837
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-05-08 13:23:18 -0600

    Created Trail class https://app.asana.com/0/660901941582663/663621423549618

---
+++ php/classes/Trail.php
@@ -0,0 +1,12 @@
+<?php
+namespace Edu\Cnm\AbqOutside;
+require_once("autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+use Ramsey\Uuid\Uuid;
+/**
+ *
+ * This is the Trail class where we find data about each trail such as Id, Extrenal Id, Address, Image, Name, Location, Summary, Ascent, Rating, Length, Latitude and Longitude.
+ *
+ * @author Jullyane Hawkins <jhawkins20@cnm.edu>
+ * @version 1.0.0
+ **/
