46f2e13df6a1f397a6e424f09feb8e42fb943f90
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-05-14 15:22:10 -0600

    created TrailTest https://app.asana.com/0/660901941582663/663621423549618

---
+++ php/classes/Test/TrailTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Edu\Cnm\AbqOutside\Test;
+use Edu\Cnm\AbqOutside\Trail;
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
+// grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+/**
+ * Full PHPUnit test for the Trail class
+ *
+ * This is a complete PHPUnit test of the Trail class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see Trail
+ * @author Jullyane Hawkins <jhawkins20@cnm.edu>
+ **/
+class TrailTest extends AbqOutsideTest {
+
+}
