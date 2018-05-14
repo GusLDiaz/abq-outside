3249c481990aa2711f1781796681117e420b40e9
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-05-14 14:58:17 -0600

    added getAllTrails https://app.asana.com/0/660901941582663/663621423549618

--- php/classes/Trail.php
+++ php/classes/Trail.php
@@ -561,6 +561,34 @@ class Trail implements \JsonSerializable {
 		return($trails);
 	}
 	/**
+	 * gets all Trails
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @return \SplFixedArray SplFixedArray of Trails found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+	public static function getAllTrails(\PDO $pdo) : \SPLFixedArray {
+		// create query template
+		$query = "SELECT trailId, trailExternalId, trailAddress, trailImage, trailName, trailLocation, trailSummary, trailAscent, trailRating, trailLength, trailLat, trailLong FROM trail";
+		$statement = $pdo->prepare($query);
+		$statement->execute();
+		// build an array of trails
+		$trails = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$trail = new Trail($row["trailId"], $row["trailExternalId"], $row["trailAddress"], $row["trailImage"], $row["trailName"], $row["trailLocation"], $row["trailSummary"], $row["trailAscent"], $row["trailRating"], $row["trailLength"], $row["trailLat"], $row["trailLong"]);
+				$trails[$trails->key()] = $trail;
+				$trails->next();
+			} catch(\Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return ($trails);
+	}
+	/**
 	 * formats the state variables for JSON serialization
 	 *
 	 * @return array resulting state variables to serialize
