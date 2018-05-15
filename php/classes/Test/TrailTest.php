46fa976e6dd45d184d666000565d6372181a619e
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-05-15 13:42:09 -0600

    Added the attributes to the trail unit test https://app.asana.com/0/660901941582663/663621423549618

--- php/classes/Test/TrailTest.php
+++ php/classes/Test/TrailTest.php
@@ -16,10 +16,15 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
  **/
 class TrailTest extends AbqOutsideTest {
 	/**
+	 * external id of this trail
+	 * @var string $VALID_TRAILEXTERNALID
+	 **/
+	protected $VALID_TRAILEXTERNALID = "313572";
+	/**
 	 * address of this trail
 	 * @var string $VALID_TRAILADDRESS
 	 **/
-	protected $VALID_TRAILADDRESS = "7601 St Josephs Ave, Albuquerque, NM 87120";
+	protected $VALID_TRAILADDRESS = "13 Reel Avenue, Albuquerque, NM 87111";
 	/**
 	 * address of this trail
 	 * @var string $VALID_TRAILADDRESS2
@@ -29,10 +34,45 @@ class TrailTest extends AbqOutsideTest {
 	 * trail image
 	 * @var string $VALID_TRAILIMAGE
 	 **/
-	protected $VALID_TRAILIMAGE = "http://www.protrails.com/protrails/trails/243.jpg";
+	protected $VALID_TRAILIMAGE = "https://i.ytimg.com/vi/pwFKqTGHbaU/maxresdefault.jpg";
 	/**
 	 * name of this trail
 	 * @var string $VALID_TRAILNAME
 	 **/
-	protected $VALID_TRAILNAME = "Rinconada Canyon Trail";
+	protected $VALID_TRAILNAME = "Variable Valley";
+	/**
+	 * location of this trail
+	 * @var string $VALID_TRAILLOCATION
+	 **/
+	protected $VALID_TRAILLOCATION = "Labyrinth of Loops";
+	/**
+	 * summary of this trail
+	 * @var string $VALID_TRAILSUMMARY
+	 **/
+	protected $VALID_TRAILSUMMARY = "The Variable Valley offers an insight into the natural resources of this region.";
+	/**
+	 * ascent of this Trail
+	 * @var int $VALID_TRAILASCENT
+	 **/
+	protected $VALID_TRAILASCENT = 40;
+	/**
+	 * rating of this Trail
+	 * @var int $VALID_TRAILRATING
+	 **/
+	protected $VALID_TRAILRATING = 4;
+	/**
+	 * length of this trail
+	 * @var float $VALID_TRAILLENGTH
+	 **/
+	protected $VALID_TRAILLENGTH = 3.14;
+	/**
+	 * latidude of this trail
+	 * @var float $VALID_TRAILLATITUDE
+	 **/
+	protected $VALID_TRAILLATITUDE = 48;
+	/**
+	 * longitude of this trail
+	 * @var float $VALID_TRAILLONGITUDE
+	 **/
+	protected $VALID_TRAILLONGITUDE = 192;
 }
