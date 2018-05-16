837dbc8c48e63b65959d057eec65bbed38369470
Gus Liakos <Gliakos@cnm.edu>
2018-05-16 16:40:19 -0600

    type concious trail constructor from api https://app.asana.com/0/660901941582663/663621423549619

--- php/classes/DataDownloader.php
+++ php/classes/DataDownloader.php
@@ -3,7 +3,8 @@
 namespace Edu\Cnm\AbqOutside;
 require_once("autoload.php");
 //require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-
+use Ramsey\Uuid\Uuid;
+require_once (dirname(__DIR__, 1) . "\lib\uuid.php");
 
 class DataDownloader {
 	/**
@@ -49,31 +50,34 @@ class DataDownloader {
 //		} catch
 //		}
 		$trailsX = self::readDataJson($urlG);
-		var_dump($trailsX);
+		//var_dump($trailsX);
 //		$pdo = getEncryptedSqlConnection("/etc/apache2/capstone-mysql/outside.ini");
-//		foreach($features as $value) {
-//			var_dump($value);
-//			$trailId = generateUuidV4();
-//			$trailExternalId = $value->id;
+
+		foreach($trailsX as $value) {
+			//var_dump($value);
+			$trailId = generateUuidV4();
+			$trailExternalId = $value->id;
 //			//$trailAddress = $value->attributes->ADDRESS;
-//			$trailAddress = "outdoors";
-//			$trailImage = $value->imgMedium;
-//			$trailName = $value->name;
-//			$trailLocation = $value->location;
-//			$trailLat = $value->latitude;
-//			$trailLong = $value->longitude;
-//			$trailLength = $value->length;
-//			$trailSummary = $value->summary;
-//			$trailAscent = $value->ascent;
-//			$trailRating = $value->stars;
-//			try {
-//				$trail = new Trail($trailId, $trailExternalId, $trailAddress, $trailImage, $trailName, $trailLocation, $trailSummary, $trailAscent, $trailRating, $trailLength, $trailLat, $trailLong);
+			$trailAddress = "outdoors";
+//			if isEmpty($value->imgMedium)
+			$trailImage = $value->imgMedium;
+			$trailName = $value->name;
+			$trailLocation = $value->location;
+			$trailLat = (float)$value->latitude;
+			$trailLong = (float)$value->longitude;
+			$trailLength = (float)$value->length;
+			$trailSummary = $value->summary;
+			$trailAscent = (int)$value->ascent;
+			$trailRating = (float)$value->stars;
+			try {
+			$trail = new Trail($trailId, $trailAddress, $trailAscent, $trailExternalId, $trailImage, $trailLat, $trailLength, $trailLocation, $trailLong, $trailName, $trailRating, $trailSummary);
+			var_dump($trail);
 //				$trail->insert($pdo);
-//			} catch(\TypeError $typeError) {
-//				//echo("Gus");
-////			}
+			} catch(\TypeError $typeError) {
+				echo("Gus");
+			}
 		}
-	//{
+	}
 
 	/** @param $url
 	 *  $url and go from there. just straight up craft this and use it
@@ -99,88 +103,4 @@ class DataDownloader {
 	}
 }
 
-echo DataDownloader::pullTrails().PHP_EOL
-
-
-//$client = new GuzzleHttp\Client{
-//['base_uri' => 'https://www.hikingproject.com/data/']
-//};
-//$response = $client->request('get-trails', 'https://www.hikingproject.com/data/get-trails?lat=35.085470&lon=-106.649072&maxDistance=200&maxResults=500&key=200265121-1809e265008042f9977e435839863103');
-//var_dump($response);
-//$data = $response->getBody();
-//echo $data;
-//$data = json_decode($data, true);
-//$cache = $data->trails;
-////count the cache
-//$trailArray = \SplFixedArray::fromArray($cache);
-
-
-//public static function getTrails(\SplFixedArray $data) {
-//	//$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/outside.ini")
-//	$pdo = getConnection("/etc/apache2/capstone-mysql/outside.ini");
-//	foreach($data as $feature) {
-//		$trailId = generateUuidV4();
-//		$trailExternalId = $feature->attributes->Id
-//			$trailAddress = $feature->attributes->ADDRESS;
-//			$trailImage = $feature->attributes->JPG_URL;
-//			$trailName = $feature->attributes->;
-//			$trailLocation = $feature->attributes->LOCATION;
-//			$trailLat = $feature->attributes->Y;
-//			$trailLong = $feature->attributes->X;
-//			$trailLength = $feature->attributes->TYPE;
-//			$trailSummary = $feature->attributes->YEAR;
-//			$trailAscent =;
-//			$trailRating =;
-//			try {
-//				$trail = new Trail($trailId, $trailExternalId, $trailAddress, $trailImage, $trailName, $trailLocation, $trailSummary, $trailAscent, $trailRating, $trailLength, $trailLat, $trailLong);
-//				$trail->insert($pdo);
-//			} catch(\TypeError $typeError) {
-//				echo("Gus");
-//			}
-//
-//}
-// = $body->getContents();
-
-//public function readDataJson($urlX) {
-//	// http://php.net/manual/en/function.stream-context-create.php creates a stream for file input
-//	$context = stream_context_create(["http" => ["ignore_errors" => true, "method" => "GET"]]);
-//	try {
-//		// http://php.net/manual/en/function.file-get-contents.php file-get-contents returns file in string context
-//		if(($jsonData = file_get_contents($urlX, null, $context)) === false) {
-//			throw(new \RuntimeException("cannot connect to city server"));
-//		}
-//		//decode the Json file
-//		$jsonConverted = json_decode($jsonData);
-//		//format
-//		$jsonFeatures = $jsonConverted->features;
-//		//create array from the converted Json file
-//		$data = \SplFixedArray::fromArray($jsonFeatures);
-//	} catch(\Exception $exception) {
-//		throw(new \PDOException($exception->getMessage(), 0, $exception));
-//	}
-//	return ($data);
-//}
-//public static function getTrails(\SplFixedArray $data) {
-//	//$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/outside.ini")
-//	$pdo = getConnection("/etc/apache2/capstone-mysql/outside.ini");
-//	foreach($data as $feature) {
-//		$trailId = generateUuidV4();
-//		$trailExternalId = $feature->attributes->Id
-//			$trailAddress = $feature->attributes->ADDRESS;
-//			$trailImage = $feature->attributes->JPG_URL;
-//			$trailName = $feature->attributes->;
-//			$trailLocation = $feature->attributes->LOCATION;
-//			$trailLat = $feature->attributes->Y;
-//			$trailLong = $feature->attributes->X;
-//			$trailLength = $feature->attributes->TYPE;
-//			$trailSummary = $feature->attributes->YEAR;
-//			$trailAscent =;
-//			$trailRating =;
-//			try {
-//				$trail = new Trail($trailId, $trailExternalId, $trailAddress, $trailImage, $trailName, $trailLocation, $trailSummary, $trailAscent, $trailRating, $trailLength, $trailLat, $trailLong);
-//				$trail->insert($pdo);
-//			} catch(\TypeError $typeError) {
-//				echo("Gus");
-//			}
-//?>
-
+echo DataDownloader::pullTrails().PHP_EOL;
