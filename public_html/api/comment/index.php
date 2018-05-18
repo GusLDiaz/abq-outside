a69d13d0437d189e516e660b81cb0e61e6315482
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-05-18 11:29:21 -0600

    Started writing Comment api https://app.asana.com/0/660901941582663/679110671431385

--- public_html/api/comment/index.php
+++ public_html/api/comment/index.php
@@ -1,7 +1,15 @@
 <?php
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\AbqOutside\{
+	Comment, Profile, Trail
+};
 /**
- * Created by PhpStorm.
- * User: jully
- * Date: 5/18/18
- * Time: 11:22 AM
- */
+ * api for the Comment class
+ *
+ * @author Jullyane Hawkins <jullyanehawkins@gmail.com>
+ **/
