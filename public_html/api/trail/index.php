bac982c82fcd67e595872686944621b0138b735a
Gus Liakos <Gliakos@cnm.edu>
2018-05-22 16:53:44 -0600

    add distance functionality. see length https://app.asana.com/0/660021943530581/679110671431386

--- public_html/api/trail/index.php
+++ public_html/api/trail/index.php
@@ -32,19 +32,19 @@ try {
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 //sanitize input (id ~profileId)
 	$trailId = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	//$trailAdddress=
-	$trailAscent = filter_input(INPUT_GET, "profileActivationToken", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$profileEmail = filter_input(INPUT_GET, "profileEmail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$profileUserName = filter_input(INPUT_GET, "profileUserName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$trailExternalId;
-	$trailImage = filter_input(INPUT_GET, "trailImage", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$trailLat = filter_input(INPUT_GET, "trailLat", FILTER_SANITIZE_NUMBER_FLOAT);
+//	$trailAddress=
+//	$trailAscent = filter_input(INPUT_GET, "profileActivationToken", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+//	$profileEmail = filter_input(INPUT_GET, "profileEmail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+//	$profileUserName = filter_input(INPUT_GET, "profileUserName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+//	$trailExternalId = filter_input(INPUT_GET, "traia", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+//	$trailImage = filter_input(INPUT_GET, "trailImage", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+//	$trailLat = filter_input(INPUT_GET, "trailLat", FILTER_SANITIZE_NUMBER_FLOAT);
 	$trailLength = filter_input(INPUT_GET, "trailLat", FILTER_SANITIZE_NUMBER_FLOAT);
-	$trailLocation = filter_input(INPUT_GET, "trailLocation", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$trailLong;
-	$trailName = filter_input(INPUT_GET, "profileActivationToken", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$trailRating;
-	$trailSummary = filter_input(INPUT_GET, "profileActivationToken", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+//	$trailLocation = filter_input(INPUT_GET, "trailLocation", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+//	$trailLong;
+//	$trailName = filter_input(INPUT_GET, "profileActivationToken", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+//	$trailRating;
+//	$trailSummary = filter_input(INPUT_GET, "profileActivationToken", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 	$distance = filter_input(INPUT_GET, "distance", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
 	$userLat = filter_input(INPUT_GET, "userLat", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
@@ -57,18 +57,16 @@ try {
 			if($trail !== null) {
 				$reply->data = $trail;
 			}
-
-		} else if(empty($trailName) === false) {
-			$trail = Trail::getTrailByDistance($pdo, $*********;
+		} else if(empty($userLat) === false && empty($userLong) === false && empty($distance) === false) {
+			$trail = Trail::getTrailByDistance($pdo, $userLong, $userLat, $distance)->toArray();
 			if($trail !== null) {
 				$reply->data = $trail;
-			}
 
-		} catch(Exception $exception) {
+		} catch(\Exception $exception) {
 			$reply->status = $exception->getCode();
 			$reply->message = $exception->getMessage();
 			$reply->trace = $exception->getTraceAsString();
-		} catch(TypeError $typeError) {
+		} catch(\TypeError $typeError) {
 			$reply->status = $typeError->getCode();
 			$reply->message = $typeError->getMessage();
 		}
