77db7e3da142b336a02a3d9ef53c5e2775c2d821
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-05-23 14:22:32 -0600

    fixed line 26 https://app.asana.com/0/660901941582663/679110671431385

--- public_html/api/comment/index.php
+++ public_html/api/comment/index.php
@@ -23,7 +23,7 @@ $reply->status = 200;
 $reply->data = null;
 try {
 	//grab the mySQL connection
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/abqoutside.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/outside.ini");
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 	//sanitize the search parameters
