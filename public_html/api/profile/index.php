cb5f777ffea2790d901622a378035d94886d60f8
Michael Figueroa <mfigueroa14@cnm.edu>
2018-05-29 16:18:51 -0600

    all methods besides get removed on profile api https://app.asana.com/0/660901941582663/679110671431383/f

--- public_html/api/profile/index.php
+++ public_html/api/profile/index.php
@@ -32,7 +32,7 @@ try {
 	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$profileEmail = filter_input(INPUT_GET, "profileEmail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	// make sure the id is valid for methods that require it
-	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true )) {
+	if(($method === "POST" || $method === "PUT") && (empty($id) === true )) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
 	if($method === "GET") {
@@ -44,61 +44,12 @@ try {
 			if($profile !== null) {
 				$reply->data = $profile;
 			}
-		} else if(empty($profileEmail) === false) {
+		} elseif(empty($profileEmail) === false) {
 			$profile = Profile::getProfileByProfileEmail($pdo, $profileEmail);
 			if($profile !== null) {
 				$reply->data = $profile;
 			}
 		}
-	} elseif($method === "PUT") {
-		//enforce that the XSRF token is present in the header
-		verifyXsrf();
-		//enforce the end user has a JWT token
-		//validateJwtHeader();
-		//enforce the user is signed in and only trying to edit their own profile
-		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $id) {
-			throw(new \InvalidArgumentException("You are not allowed to access this profile", 403));
-		}
-		validateJwtHeader();
-		//decode the response from the front end
-		$requestContent = file_get_contents("php://input");
-		$requestObject = json_decode($requestContent);
-		//retrieve the profile to be updated
-		$profile = Profile::getProfileByProfileId($pdo, $id);
-		if($profile === null) {
-			throw(new RuntimeException("Profile does not exist", 404));
-		}
-		//profile at handle
-		if(empty($requestObject->profileAtHandle) === true) {
-			throw(new \InvalidArgumentException ("No profile at handle", 405));
-		}
-		//profile email is a required field
-		if(empty($requestObject->profileEmail) === true) {
-			throw(new \InvalidArgumentException ("No profile email present", 405));
-		}
-		$profile->setProfileEmail($requestObject->profileEmail);
-		$profile->update($pdo);
-		// update reply
-		$reply->message = "Profile information updated";
-	} elseif($method === "DELETE") {
-		//verify the XSRF Token
-		verifyXsrf();
-		//enforce the end user has a JWT token
-		//validateJwtHeader();
-		$profile = Profile::getProfileByProfileId($pdo, $id);
-		if($profile === null) {
-			throw (new RuntimeException("Profile does not exist"));
-		}
-		//enforce the user is signed in and only trying to edit their own profile
-		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $profile->getProfileId()->toString()) {
-			throw(new \InvalidArgumentException("You are not allowed to access this profile", 403));
-		}
-		validateJwtHeader();
-		//delete the post from the database
-		$profile->delete($pdo);
-		$reply->message = "Profile Deleted";
-	} else {
-		throw (new InvalidArgumentException("Invalid HTTP request", 400));
 	}
 	// catch any exceptions that were thrown and update the status and message state variable fields
 } catch(\Exception | \TypeError $exception) {
