eae907af0e88c45a449d48f5d5b4db8f3efab660
Edith Thakkar <egarcia262@cnm.edu>
2018-06-05 16:53:23 -0600

    added images to carousel

--- static-ui/home-view.php
+++ static-ui/home-view.php
@@ -1,5 +1,52 @@
 <?php require_once("head-utils.php"); ?>
 <?php require_once("navbar.php"); ?>
+
+<!--Carousel-->
+<div id="carouselExampleFade" class="carousel slide" data-ride="carousel">
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img class="d-block w-100" src="../src/img/road-3133502_1280.jpg" alt="First slide">
+			<div class="carousel-caption d-none d-md-block lead">
+				<h1>Welcome</h1>
+				<p>Unleash your inner travel spirit</p>
+			</div>
+		</div>
+
+		<div class="carousel-item">
+			<img class="d-block w-100" src="carousel/lake.jpg" alt="Second slide">
+			<div class="carousel-caption d-none d-md-block lead">
+			</div>
+		</div>
+
+		<div class="carousel-item">
+			<img class="d-block w-100" src="carousel/menton-old-town-harbour-entrance-lighthouse-161098.jpg"
+				  alt="Third slide">
+			<div class="carousel-caption d-none d-md-block lead">
+			</div>
+		</div>
+
+		<div class="carousel-item">
+			<img class="d-block w-100" src="carousel/mountains.jpg" alt="Fourth slide">
+			<div class="carousel-caption d-none d-md-block lead">
+			</div>
+		</div>
+
+		<div class="carousel-item">
+			<img class="d-block w-100" src="carousel/ocean.jpg" alt="Fifth slide">
+			<div class="carousel-caption d-none d-md-block lead">
+			</div>
+		</div>
+
+		<div class="carousel-item">
+			<img class="d-block w-100" src="carousel/pexels-photo-457937.jpg" alt="Sixth slide">
+			<div class="carousel-caption d-none d-md-block lead">
+			</div>
+		</div>
+	</div>
+</div>
+
+
 <div class="col-12 col-md-8 p-3">
 	<img class="img-fluid" src="../src/img/temporary-map.jpeg" alt="map">
 </div>
+
