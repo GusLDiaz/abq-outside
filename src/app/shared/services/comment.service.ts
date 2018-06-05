6e40f3dea8f932e935bacd24fa662de5e9a083d2
Edith Thakkar <egarcia262@cnm.edu>
2018-06-05 14:33:29 -0600

    added calls for comment

--- src/app/shared/services/comment.service.ts
+++ src/app/shared/services/comment.service.ts
@@ -6,3 +6,42 @@ import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class CommentService {
+
+
+//define the API endpoint
+	private commentUrl = "api/comment/";
+
+	constructor(protected http: HttpClient) {
+	}
+
+//call the Comment API and create a new comment
+	createComment(comment: Comment): Observable<Status> {
+		return (this.http.post<Status>(this.commentUrl, comment));
+	}
+
+// call to the Comment API and get a Comment object by its primary key, id
+	getCommentByCommentId(id: string): Observable<Comment> {
+		return (this.http.get<Comment>(this.commentUrl + id));
+	}
+
+// call to the Comment API and get a Comment object by its foreign key, profile id
+	getCommentByCommentProfileId(commentProfileId: string): Observable<Comment[]> {
+		return (this.http.get<Comment[]>(this.commentUrl + commentProfileId));
+
+	}
+
+// call to the Comment API and get a Comment object by its foreign key,  trail id
+	getCommentByCommentTrailId(commentTrailId: string): Observable<Comment[]> {
+		return (this.http.get<Comment[]>(this.commentUrl + commentTrailId));
+	}
+
+// call to the Comment API and get a Comment content
+	getCommentByCommentContent(commentContent: string): Observable<Comment[]> {
+		return (this.http.get<Comment[]>(this.commentUrl + commentContent));
+	}
+
+// call to the Comment API and get a Comment timestamp
+	getCommentByCommentTimestamp(commentTimestamp: string): Observable<Comment> {
+		return (this.http.get<Comment[]>(this.commentUrl + commentTimestamp));
+	}
+}
