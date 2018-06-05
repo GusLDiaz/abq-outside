79689f7aa775724057ab4a680509fe3052593e2d
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-06-05 15:14:10 -0600

    Added trail service and class https://app.asana.com/0/660901941582663/698758989766687

---
+++ src/app/shared/classes/trail.ts
@@ -0,0 +1,16 @@
+export class Trail {
+	constructor(
+		public trailId: string,
+		public trailAddress: string,
+		public trailAscent: number,
+		public trailExternalId: string,
+		public trailImage: string,
+		public trailLat: number,
+		public trailLength: number,
+		public trailLocation: string,
+		public trailLong: number,
+		public trailName: string,
+		public trailRating: number,
+		public trailSummary: string
+	) {}
+}
--- src/app/shared/services/trail.service.ts
+++ src/app/shared/services/trail.service.ts
@@ -3,4 +3,25 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs/Observable";
 import {Trail} from "../classes/trail";
 import {BehaviorSubject} from "rxjs/BehaviorSubject";
-
+@Injectable()
+export class TrailService {
+	protected trailSubject : BehaviorSubject<Trail[]> = new BehaviorSubject<Trail[]>([]);
+	public trailObserver : Observable<Trail[]> = this.trailSubject.asObservable();
+	constructor(protected http: HttpClient) {
+		this.getAllTrails().subscribe(trails => this.trailSubject.next(trails));
+	}
+	//define the API endpoint
+	private trailUrl = "api/trail/";
+	// call to the Trail API and get a Trail object by its id
+	getTrailByTrailId(id : string): Observable<Trail> {
+		return (this.http.get<Trail>(this.trailUrl + id));
+	}
+	// call to the Trail API and get a Trail object by its distance
+	getTrailByDistance(distance: string): Observable<Trail[]> {
+		return (this.http.get<Trail[]>(this.trailUrl, {params: new HttpParams().set("distance", distance)}));
+	}
+	// call to the Trail API and get all Trails
+	getAllTrails(): Observable<Trail[]> {
+		return (this.http.get<Trail[]>(this.trailUrl));
+	}
+}
