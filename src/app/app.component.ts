cdca03336bc59da008a8114b324a47ddc057d8ea
Gus Liakos <Gliakos@cnm.edu>
2018-06-05 15:13:43 -0600

    early building angular https://app.asana.com/0/660901941582663/698758989766686

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from "@angular/core";
+// import {Status} from "./shared/classes/status";
+// import {SessionService} from "./shared/services/session.service";
+
+@Component({
+	selector: "cool-app",
+	template: require("./app.component.html")
+})
+export class AppComponent{
+
+
+
+	// status : Status = null;
+	//
+	// constructor(protected sessionService : SessionService) {
+	// 	this.sessionService.setSession()
+	// 		.subscribe(status => this.status = status);
+	// }
+}
