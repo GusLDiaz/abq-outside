171bfebfbf03938dba6f22a1e4440b48a3ed42d2
Edith Thakkar <egarcia262@cnm.edu>
2018-06-05 13:44:49 -0600

    added comment.ts and comment.service.ts

---
+++ src/app/shared/classes/comment.ts
@@ -0,0 +1,4 @@
+export class Comment {
+	constructor(public id: string, public commentId: string, public commentProfileId: string, public commentTrailId: string, public commentContent: string, public commentTimestamp: string) {
+	}
+}
