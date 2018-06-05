c2346100f24e2f1d37475c44ef55d5b8f2bc031b
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-06-05 16:34:10 -0600

    Navbar is ready https://app.asana.com/0/660901941582663/698758989766688

--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -1,1 +1,24 @@
 <?php require_once ("head-utils.php");?>
+<header>
+	<div class="container-fluid p-3 mb-2 bg-success text-white">
+		<nav class="navbar navbar-expand-lg">
+			<img src="../src/img/temporary-logo.jpeg" alt="logo" class="logo align-center mx-auto">
+			<button class="navbar-toggler navbar-toggler-right navbar-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a href="about-us.php" class="nav-link text-white">About Us</a>
+					</li>
+					<li class="nav-item">
+						<a href="https://bootcamp-coders.cnm.edu/~egarcia262/abq-outside/public_html/api/oauth" class="nav-link text-white">Log in with GitHub</a>
+					</li>
+					<li class="nav-item">
+						<a href="trails.php" class="nav-link text-white">Trails</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+</header>
