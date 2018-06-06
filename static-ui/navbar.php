70294969c438b76c6862c92ab44e31db1b769a99
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-06-06 09:09:24 -0600

    Did some style on the navbar AGAIN https://app.asana.com/0/660901941582663/698758989766688

--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -2,25 +2,25 @@
 <header>
 	<div class="container-fluid p-3 mb-2 bg-success text-white">
 		<nav class="navbar navbar-expand-lg">
-			<img src="../src/img/temporary-logo.jpeg" alt="logo" class="logo align-center mx-auto">
+			<img src="../src/img/temporary-logo.jpeg" alt="logo" class="img-fluid rounded-circle logo align-center mx-auto">
 			<button class="navbar-toggler navbar-toggler-right navbar-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav mr-auto">
 					<li class="nav-item">
-						<a href="home-view.php" class="nav-link text-dark rounded border bg-light text-center">Home</a>
+						<a href="home-view.php" class="nav-link text-dark font-weight-bold rounded border bg-light text-center">Home</a>
 					</li>
 					<li class="nav-item">
-						<a href="about-us.php" class="nav-link text-dark rounded border bg-light text-center">About Us</a>
+						<a href="about-us.php" class="nav-link text-dark font-weight-bold rounded border bg-light text-center">About Us</a>
 					</li>
 					<li class="nav-item">
-						<a href="trails.php" class="nav-link text-dark rounded border bg-light text-center">Trails</a>
+						<a href="trails.php" class="nav-link text-dark font-weight-bold rounded border bg-light text-center">Trails</a>
 					</li>
 				</ul>
 				<ul class="navbar-nav ml-auto">
 					<li class="nav-item">
-						<a href="https://bootcamp-coders.cnm.edu/~egarcia262/abq-outside/public_html/api/oauth" class="nav-link text-dark rounded border bg-light text-center">Log in with GitHub</a>
+						<a href="https://bootcamp-coders.cnm.edu/~egarcia262/abq-outside/public_html/api/oauth" class="nav-link text-dark font-weight-bold rounded border bg-light text-center">Log in with GitHub</a>
 					</li>
 				</ul>
 			</div>
