b1c7e86cca4191bbb0f65ac3b5ae4ff6844f49f2
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-06-06 09:46:49 -0600

    Edited about us page https://app.asana.com/0/660901941582663/698758989766688

--- static-ui/about-us.php
+++ static-ui/about-us.php
@@ -1,2 +1,51 @@
 <?php require_once("head-utils.php"); ?>
 <?php require_once("navbar.php"); ?>
+<h1>About Us</h1>
+<div class="py-5 bg-light">
+	<div class="container" id="about-me">
+		<div class="row">
+			<div class="column-fluid col-md-2 bg-light">
+				<img class="img-fluid rounded-circle" src="../src/img/edith.jpeg" alt="Edith Thakkar">
+			</div>
+			<div class="column-fluid mt-5 col 2">
+				<h1 class="about">Edith Thakkar</h1>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="py-5 bg-light">
+	<div class="container" id="about-me">
+		<div class="row">
+			<div class="column-fluid col-md-2 bg-light">
+				<img class="img-fluid rounded-circle" src="../src/img/gus.jpeg" alt="Gus Liakos">
+			</div>
+			<div class="column-fluid mt-5 col 2">
+				<h1 class="about">Gus Liakos</h1>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="py-5 bg-light">
+	<div class="container" id="about-me">
+		<div class="row">
+			<div class="column-fluid col-md-2 bg-light">
+				<img class="img-fluid rounded-circle" src="../src/img/jully.jpeg" alt="Jullyane Hawkins">
+			</div>
+			<div class="column-fluid mt-5 col 2">
+				<h1 class="about">Jullyane Hawkins</h1>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="py-5 bg-light">
+	<div class="container" id="about-me">
+		<div class="row">
+			<div class="column-fluid col-md-2 bg-light">
+				<img class="img-fluid rounded-circle" src="../src/img/michael.jpeg" alt="Michael Figueroa">
+			</div>
+			<div class="column-fluid mt-5 col 2">
+				<h1 class="about">Michael Figueroa</h1>
+			</div>
+		</div>
+	</div>
+</div>
