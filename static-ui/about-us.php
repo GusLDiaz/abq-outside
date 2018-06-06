edfdfa05134a3fae16170eca52b97a862234f1ce
Edith Thakkar <egarcia262@cnm.edu>
2018-06-06 11:39:45 -0600

    added text placeholder, and links to git hub accounts under about us section

--- static-ui/about-us.php
+++ static-ui/about-us.php
@@ -9,6 +9,14 @@
 			</div>
 			<div class="column-fluid mt-5 col 2">
 				<h1 class="about">Edith Thakkar</h1>
+				<p>Every day is taco ipsum tuesday. Burritos are very tasty. I’d have to say, those tacos are on fleek. Say
+					taco one more time. Tacos for breakfast, lunch and dinner. Let’s do a beef and a chicken, and one with
+					both. If you were a taco, would you eat yourself? How bout a gosh darn quesadilla? It’s taco Tuesday
+					Monday. Josh’s taco shack is the best taco shack. TACOS!! Yeah, apparently the taco shack was robbed.
+					They left the money but took the tacos.</p>
+				<p>
+					<a href="https://github.com/ediththakkar" target="_blank">Git Hub</a>
+				</p>
 			</div>
 		</div>
 	</div>
@@ -21,6 +29,13 @@
 			</div>
 			<div class="column-fluid mt-5 col 2">
 				<h1 class="about">Gus Liakos</h1>
+				<p>Give me all the tacos, immediately. Give me all your tacos. Yeah, apparently the taco shack was robbed.
+					They left the money but took the tacos. Josh’s taco shack is the best taco shack. Tacos, tacos, tacos.
+					Tacos Al pastor/De Adobada are made of thin pork steaks seasoned with adobo seasoning, then skewered and
+					overlapped on one another on a vertical rotisserie cooked and flame-broiled as it spins. It’s a wonderful
+					morning for breakfast tacos. Let’s do a beef and a chicken, and one with both. Let’s do a beef and a
+					chicken, and one with both.</p>
+				<p><a href="https://github.com/GusLDiaz" target="_blank">Git Hub</a></p>
 			</div>
 		</div>
 	</div>
@@ -33,6 +48,13 @@
 			</div>
 			<div class="column-fluid mt-5 col 2">
 				<h1 class="about">Jullyane Hawkins</h1>
+				<p>Tacos dorados called flautas, or taquitos, for which the tortillas are filled with pre-cooked shredded
+					chicken, beef or barbacoa, rolled into an elongated cylinder and deep-fried until crisp. Josh’s taco
+					shack is the best taco shack. Tacos for breakfast, lunch and dinner. Tacos dorados called flautas, or
+					taquitos, for which the tortillas are filled with pre-cooked shredded chicken, beef or barbacoa, rolled
+					into an elongated cylinder and deep-fried until crisp. It’s taco time all the time. Make it a double
+					there pal.</p>
+				<p><a href="https://github.com/jullyanehawkins" target="_blank">Git Hub</a></p>
 			</div>
 		</div>
 	</div>
@@ -45,6 +67,12 @@
 			</div>
 			<div class="column-fluid mt-5 col 2">
 				<h1 class="about">Michael Figueroa</h1>
+				<p>These tacos are lit 🔥. Tacos, again? This will be 5 times this week and it’s only Tuesday. Give me
+					tacos, or give me death. Tacos, tacos, tacos. These tacos are lit 🔥. Yeah, apparently the taco shack was
+					robbed. They left the money but took the tacos. Tacos al pastor made with adobada meat. Fish tacos with
+					cabbage slaw and a side of chips and guac. It’s a wonderful morning for breakfast tacos. Shrimp tacos are
+					tasty tacos!</p>
+				<p><a href="https://github.com/Mike1f" target="_blank">Git Hub</a></p>
 			</div>
 		</div>
 	</div>
