5a8888687a4346caca2e99cad29a626c94e081a3
Gus Liakos <Gliakos@cnm.edu>
2018-06-06 14:24:20 -0600

    kill whitespace https://app.asana.com/0/660901941582663/698758989766688

--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -1,6 +1,6 @@
 <?php require_once("head-utils.php"); ?>
 <header>
-	<div class="container-fluid p-3 mb-2 bg-success text-white">
+	<div class="container-fluid p-3 bg-success text-white">
 		<nav class="nav navbar-expand-lg  nav-pills flex-column">
 			<button class="navbar-toggler navbar-toggler-right navbar-dark" type="button" data-toggle="collapse"
 					  data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -9,10 +9,11 @@
 			</button>
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav mr-auto">
-					<li class="nav-item">
+					<li class="nav-item pr-2">
 						<a href="home-view.php"
 							class="nav-link text-dark font-weight-bold rounded border bg-light text-center">Home</a>
 					</li>
+
 					<li class="nav-item">
 						<a href="about-us.php"
 							class="nav-link text-dark font-weight-bold rounded border bg-light text-center">About Us</a>
