bed73978bb5695942c25c901299f5a402940a477
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-06-06 10:19:55 -0600

    We need to move the logo and the map to the center of the page https://app.asana.com/0/660901941582663/698758989766688

--- static-ui/home-view.php
+++ static-ui/home-view.php
@@ -1,7 +1,5 @@
 <?php require_once("head-utils.php"); ?>
 <?php require_once("navbar.php"); ?>
-
-<!--Carousel-->
 <div id="carouselExampleFade" class="carousel slide" data-ride="carousel">
 	<div class="carousel-inner">
 		<div class="carousel-item active">
@@ -9,20 +7,17 @@
 			<div class="carousel-caption d-none d-md-block lead">
 			</div>
 		</div>
-
 		<div class="carousel-item">
 			<img class="d-block w-100" src="../src/img/sunset.jpg" alt="Second slide">
 			<div class="carousel-caption d-none d-md-block lead">
 			</div>
 		</div>
-
 		<div class="carousel-item">
 			<img class="d-block w-100" src="../src/img/1500-500-PT-Geres-Trail-1500x500.jpg"
 				  alt="Third slide">
 			<div class="carousel-caption d-none d-md-block lead">
 			</div>
 		</div>
-
 		<div class="carousel-item">
 			<img class="d-block w-100" src="../src/img/1500-500-CH-Lötschen-06-1500x500.jpg" alt="Fourth slide">
 			<div class="carousel-caption d-none d-md-block lead">
@@ -31,10 +26,6 @@
 			</div>
 		</div>
 	</div>
-</div>
-
-
 <div class="col-12 col-md-8 p-3">
 	<img class="img-fluid" src="../src/img/temporary-map.jpeg" alt="map">
 </div>
-
