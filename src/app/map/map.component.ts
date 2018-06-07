60f256c222011ca686953466e884e3f064b15c45
Gus Liakos <Gliakos@cnm.edu>
2018-06-07 14:49:53 -0600

    work with map and home to create a debuggable operation https://app.asana.com/0/660901941582663/698758989766687

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,13 +1,4 @@
-import {Component, OnInit} from "@angular/core";
-import {ActivatedRoute, Params} from "@angular/router";
-import {Status} from "../classes/status";
-import {Observable} from "rxjs";
-import "rxjs/add/observable/from";
-import "rxjs/add/operator/switchMap";
-import 'rxjs/add/observable/of';
-import {ArtService} from "../services/art.service";
-import {Art} from "../classes/art";
-import {Point} from "../classes/point";
+import {Component} from "@angular/core";
 
 
 @Component({
@@ -15,35 +6,9 @@ import {Point} from "../classes/point";
 	selector: "map"
 })
 
-export class MapComponent implements OnInit {
+export class MapComponent{
 
-	// empty array of lat/long points
-	art: Art = new Art(null, null, null, null, null, null, null, null, null, null);
-	arts: Art[] = [];
-	data: Observable<Array<Art[]>>;
-	point: any;
+	constructor(){}
 
-	constructor(
-		protected artService : ArtService) {}
 
-	ngOnInit() : void {
-		this.listArts();
 	}
-
-	listArts() : any {
-		this.artService.artObserver
-			.subscribe(arts => this.arts = arts);
-	}
-
-	clicked({target: marker} : any, art : Art) {
-		this.art = marker;
-		marker.nguiMapComponent.openInfoWindow('art-details', marker);
-	}
-	hideMarkerInfo() {
-		this.point.display = !this.point.display;
-	}
-
-	displayArt(art: Art) {
-		this.art = art;
-	}
-}
---
+++ src/app/trail/trail.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core"
+
+@Component ({
+	template: require("./trail/trail.component.html")
+})
+
+export class TrailComponent {
+	constructor(protected Trail***) {
+	}
+
+	trailUp() {
+
+	}
+}
