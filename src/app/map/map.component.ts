af9434ebfc74f8a8bd7d6156754bd70d08349420
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-06-07 14:52:38 -0600

    Added map component ts https://app.asana.com/0/660901941582663/698758989766687

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,13 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
+import {Status} from "../classes/status";
+import {Observable} from "rxjs";
+import "rxjs/add/observable/from";
+import "rxjs/add/operator/switchMap";
+import 'rxjs/add/observable/of';
+import {TrailService} from "../services/trail.service";
+import {Trail} from "../classes/trail";
+import {Point} from "../classes/point";
 
 
 @Component({
@@ -6,9 +15,35 @@ import {Component} from "@angular/core";
 	selector: "map"
 })
 
-export class MapComponent{
+export class MapComponent implements OnInit {
 
-	constructor(){}
+	// empty array of lat/long points
+	trail: Trail = new Trail(null, null, null, null, null, null, null, null, null, null);
+	trails: Trail[] = [];
+	data: Observable<Array<Trail[]>>;
+	point: any;
 
+	constructor(
+		protected trailService : TrailService) {}
 
+	ngOnInit() : void {
+		this.listTrails();
 	}
+
+	listTrails() : any {
+		this.trailService.trailObserver
+			.subscribe(trails => this.trails = trails);
+	}
+
+	clicked({target: marker} : any, trail : Trail) {
+		this.trail = marker;
+		marker.nguiMapComponent.openInfoWindow('trail-details', marker);
+	}
+	hideMarkerInfo() {
+		this.point.display = !this.point.display;
+	}
+
+	displayTrail(trail: Trail) {
+		this.trail = trail;
+	}
+}
