0955275a19cec5fdb1ed5df4de7e3dadce9bf6c2
Gus Liakos <Gliakos@cnm.edu>
2018-06-11 15:35:30 -0600

    redo home component for side panel https://app.asana.com/0/660901941582663/698758989766687

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,34 +1,29 @@
 import {Component, OnInit} from "@angular/core";
 import {TrailService} from "../shared/services/trail.service";
 import {Trail} from "../shared/classes/trail";
-
+import {Router} from "@angular/router";
 @Component({
 	template: require("./home.component.html"),
 	selector: "home"
 })
-
 export class HomeComponent implements OnInit{
 	trail : string = "";
 	trails : Trail[] = [];
-	trailTipName : string = "";
-	constructor(protected trailService : TrailService){
+	detailedTrail : Trail = new Trail(null,null,null,null,null,null,null,null,null,null,null,null);
+	constructor(protected trailService : TrailService, protected router: Router){ //protected v private v public?
+
 	}
 	ngOnInit() : void {
 		this.trailService.getAllTrails()
 			.subscribe(trails => this.trails = trails);
 	}
-	showSidePanel(trail: Trail) : void {
-
-		this.trail = trail.trailId;
-		this.trailTipName = trail.trailName;
-
+	showSidePanel( trail : Trail) {
+	this.detailedTrail = trail;
+	}
+	goToTrail(){
+		this.router.navigate(['trail/' + this.detailedTrail.trailId]);
 	}
 }
-// var script = <HTMLScriptElement>document.getElementsByName("script")[0];
-// <!-- Note: A custom script is used to activate tooltips:-->
-//
-// $(function(){
-//   $('[data-toggle="tooltip"]').tooltip();
-// });
+
 
 
