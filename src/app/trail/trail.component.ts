5ec5c1c0171473b64603330a9f938c3498f5ad14
Gus Liakos <Gliakos@cnm.edu>
2018-06-12 10:48:01 -0600

    demo comment integration @session https://app.asana.com/0/660901941582663/698758989766686

--- src/app/trail/trail.component.ts
+++ src/app/trail/trail.component.ts
@@ -4,20 +4,46 @@ import {Trail} from "../shared/classes/trail";
 import {CommentService} from "../shared/services/comment.service";
 import {Comment} from "../shared/classes/comment";
 import {ActivatedRoute} from "@angular/router";
+
 @Component({
 	template: require("./trail.component.html")
 })
 export class TrailComponent implements OnInit {
-	trail : Trail = new Trail(null,null,null,null,null,null,null,null,null,null,null,null);
-	trails : Trail[] = [];
-	comments : Comment[] = [];
-	constructor(protected trailService: TrailService, protected commentService: CommentService, protected route : ActivatedRoute) {
+	trail: Trail = new Trail(null, null, null, null, null, null, null, null, null, null, null, null);
+	trails: Trail[] = [];
+	comments: Comment[] = [];
+	trailId = this.route.snapshot.params.trailId;
+	comment: Comment = new Comment(null, null, null, null, null, null);
+
+
+	constructor(protected trailService: TrailService, protected commentService: CommentService, protected route: ActivatedRoute) {
 	}
+
 	ngOnInit() {
-		let trailId = this.route.snapshot.params.trailId;
-		this.trailService.getTrailByTrailId(trailId).subscribe(reply => this.trail = reply);
-		//this.trailService.getAllTrails().subscribe(trail => this.trails = trails)
-		//this.trail = trail.trailId;
+
+		this.trailService.getTrailByTrailId(this.trailId).subscribe(reply => this.trail = reply);
+		this.commentService.getCommentByCommentTrailId(this.trailId).subscribe(this.comments => this.comments = reply);
 
 	}
+	createComment() : any {
+		if(!this.) {
+			return false
+		}
+
+		let newCommentProfileId = this.();
+
+		let comment = new Comment(null, null,null, null, this.commentCreator.value.commentContent, null);
+
+		this.commentService.createComment(comment)
+			.subscribe(status => {
+				this.status = status;
+				if(status.status === 200) {
+					this.listComments();
+					this.createCommentForm.reset();
+				} else {
+					return false
+				}
+			})
+	}
+
 }
