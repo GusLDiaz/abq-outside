3f34728418e641f7e35c99fece240a4173a2a18f
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-06-12 14:44:32 -0600

    Added some style to the comment form and fixed bugs from trail component ts

--- src/app/trail/trail.component.ts
+++ src/app/trail/trail.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit} from "@angular/core"
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {TrailService} from "../shared/services/trail.service";
 import {Trail} from "../shared/classes/trail";
 import {CommentService} from "../shared/services/comment.service";
 import {Comment} from "../shared/classes/comment";
 import {ActivatedRoute} from "@angular/router";
+import {Status} from "../shared/classes/status";
 
 @Component({
 	template: require("./trail.component.html")
@@ -14,38 +16,36 @@ export class TrailComponent implements OnInit {
 	comments: Comment[] = [];
 	trailId = this.route.snapshot.params.trailId;
 	comment: Comment = new Comment(null, null, null, null, null, null);
+	commentCreator: FormGroup;
+	status: Status = null;
 
-
-	constructor(protected trailService: TrailService, protected commentService: CommentService, protected route: ActivatedRoute) {
+	constructor(protected trailService: TrailService, protected formBuilder: FormBuilder, protected commentService: CommentService, protected route: ActivatedRoute) {
 	}
 
 	ngOnInit() {
-
+		// showComments : void {
+		//
+		// }
 		this.trailService.getTrailByTrailId(this.trailId).subscribe(reply => this.trail = reply);
-		var commentTrailId = this.commentService.getCommentByCommentTrailId(this.trailId);
+		let commentTrailId = this.commentService.getCommentByCommentTrailId(this.trailId);
 		commentTrailId.subscribe(comments => this.comments = comments);
-
+		this.commentCreator = this.formBuilder.group({
+			commentContent: ["", [Validators.maxLength(2000), Validators.required]]
+		});
 	}
-	// createComment() : any {
-	// 	if(!this.event) {
-	// 		return false
-	// 	}
-	//
-	// 	let comment = new Comment(null, null,null, null, this.commentCreator.value.commentContent, null);
-	//
-	// 	this.commentService.createComment(comment)
-	// 		.subscribe(status =>
-	// 			this.status = status);
-	// 			if(status.status === 200) {
-	// 				this.Speak
-	// 				this.createCommentForm.reset();
-	// 			} else {
-	// 				return false
-	// 			}
-	// 		})
-	// }
-	// 	speak() {
-	//
-	// 		let commentTrailId : string = this.route.snapshot.params["commentArtId"];
-	// 	}
+
+	createComment(): any {
+
+		let comment = new Comment(null, null, null, null, this.createComment().value.commentContent, null);
+
+		this.commentService.createComment(comment)
+			.subscribe(status =>
+				this.status = status);
+		// if(status.status === 200) {
+		// 	this.showComments();
+		// 	this.createComment.reset();
+		// } else {
+		// 	return false
+		// }
+	}
 }
