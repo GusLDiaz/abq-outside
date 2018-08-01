16ffa56633ebc602a2124b927e2f1c9f233fcd11
Gus Liakos <Gliakos@cnm.edu>
2018-07-31 20:37:42 -0600

    Back from the dead

--- php/classes/DataDownloader.php
+++ php/classes/DataDownloader.php
@@ -7,37 +7,7 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once (dirname(__DIR__, 1) . "/lib/uuid.php");
 
 class DataDownloader {
-	/**
-	 * https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200265
-	 * two options: GetTrails, GetTrailsByTrailId
-	 *   method Get trails:
-	 *
-	 *Required Args:
-	 * key: your private key ; Lat (4decimal) ; Lon (4decimal)
-	 *
-	 *Optional Args:
-	 * maxDistance - Max distance, in miles, from lat, lon. Default: 30. Max: 200
-	 * maxResults - Max number of trails to return. Default: 10. Max: 500.
-	 * sort - Values can be 'quality', 'distance'. Default: quality.
-	 * minLength - Min trail length, in miles. Default: 0 (no minimum).
-	 * minStars - Min star rating, 0-4. Default: 0.
-	 *
-	 * URL:  basics + get-trails (method) + ? + "Lat=" + latitude + "Lon=" + longitude + "&" (ampersand) + "maxDistance=" + distance + "&"(ampersand)+ "key=" + private key from HP API
-	 * EX: "https://www.hikingproject.com/data/get-trails" + "?" + "lat=" + "40.0274" + "&" + "lon=" + "-105.2519" + "&" + "maxDistance=" + "10" +"&" + "key=" + "200265121-1809e265008042f9977e435839863103"
-	 * EX: https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200265121-1809e265008042f9977e435839863103
-	 *
-	 *   method GetTrailsById:
-	 *
-	 * Required Arguments:
-	 * key - Your private key
-	 * ids - one or more trail IDs, separated by commas
-	 *
-	 * URL:  basics + get-trails-by-id (method) + ? + "ids=" + trailExternalId (s) --char7 seperated by commas + "&" ampersand + "key=" + private key from HP API
-	 * EX: "https://www.hikingproject.com/data/get-trails-by-id" + "?" + "ids=7000108,7002175,7005207,7001726,7005428" + "&" + "key=200265121-1809e265008042f9977e435839863103"
-	 *      https://www.hikingproject.com/data/get-trails-by-id?ids=7000108,7002175,7005207,7001726,7005428&key=200265121-1809e265008042f9977e435839863103
-	 *
-	 * |||||||
-	 */
+	//Test Edit
 
 	public static function pullTrails() {
 		$trailsX = null;
